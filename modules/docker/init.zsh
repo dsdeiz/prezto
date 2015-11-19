52ee118f6f90f7c9a861653ec2fabcb1bc7d165d
Arnold French <arnold@prometsource.com>
2015-11-20 07:17:13 +0800

    Remove b2d stuffs.

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -7,18 +7,10 @@
 
 # Only setup the boot2docker commands and aliases if we're on OSX
 if [[ "$OSTYPE" == darwin* ]]; then
-
-    # Set environment if boot2docker is already running
-    if [[ $(boot2docker status) == "running" ]]; then
-        eval $(boot2docker shellinit 2> /dev/null)
-    fi
-    
-    alias b2d='boot2docker'
-    alias b2dstart='boot2docker start &> /dev/null && eval $(boot2docker shellinit 2> /dev/null)'
-    alias b2dstop='boot2docker stop'
-    alias b2dssh='boot2docker ssh'
-    alias b2di='boot2docker info'
-    
+    alias dm='docker-machine'
+    alias dmstart='docker-machine start'
+    alias b2dstop='docker-machine stop'
+    alias b2dssh='docker-machine ssh'
 fi
 
 
@@ -32,4 +24,3 @@ docbash() {
     docker exec -t -i "$1" /bin/bash
 }
 compdef -e 'words[1]=(docker exec); service=docker; (( CURRENT+=1 )); _docker' docbash
-
